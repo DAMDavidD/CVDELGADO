32f69b795bde09d0c2538a11b4f3df61adcbf550
dam <dam@203P19>
2023-02-17 10:06:12 +0100

    Líneas del método main

---
+++ src/main/java/com/cvdelgado/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cvdelgado;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("dos líneas");
+        System.out.println("Estoy en la rama produccion");
+    }
+}
